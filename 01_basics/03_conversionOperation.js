3c37256a36495cbc2997a4b412d52b0044ceab5c
Gaurav Dwivedi <gauravdwivedi37@gmail.com>
2024-04-19 08:50:17 +0530

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,74 @@
+let score = "29gd"
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber);
+
+
+// 20 => 20
+// "20ab" => NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn)
+// true => 1
+// false => 0
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+
+
+// **************************** Operations *******************************
+
+let value = 5
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2) // addition
+// console.log(2*2) // multiply
+// console.log(2-2) // subtraction
+// console.log(2**3) // power
+// console.log(2/2) // devide
+// console.log(2%2) // remainder
+
+let str1 = "Hello"
+let str2 = " Gaurav"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(+true)
+// console.log(+"")
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
+
+
+
+
