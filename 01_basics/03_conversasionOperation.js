321c9da88313650febb27cca3e40b35366c196a4
Gaurav Dwivedi <gauravdwivedi37@gmail.com>
2024-04-19 08:29:56 +0530

    conversion of data types

---
+++ 01_basics/03_conversasionOperation.js
@@ -0,0 +1,27 @@
+let score = "29gd"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+
+// 20 => 20
+// "20ab" => NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof(booleanIsLoggedIn));
+console.log(booleanIsLoggedIn)
+// true => 1
+// false => 0
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
