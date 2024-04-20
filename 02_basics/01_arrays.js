a42b17aff0fdd7d982244d81cf959ab2da8d37a0
Gaurav Dwivedi <gauravdwivedi37@gmail.com>
2024-04-20 12:22:56 +0530

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,29 @@
+// Array
+
+const myArr = [1,2,3,4,5,6,"gaurav", true] // array could be mixed
+
+const heros = ["Batman", "Junior G", "Spiderman"] // array could be strings value
+
+const arr2 = new Array(9,8,7,4,5,6) // new way to create array
+
+// console.log(heros[1]) this is how we extract value through indexing
+
+// Array Methods
+
+myArr.push(6) // adding element
+// console.log(myArr)
+myArr.pop() // remove last element
+// console.log(myArr)
+
+myArr.unshift(10) // add value in first index
+// console.log(myArr)
+
+
+
+
+
+
+
+
+
+
