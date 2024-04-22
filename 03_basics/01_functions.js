00e727292a5f9a8b71a9354e0c97229e292e24a6
Gaurav Dwivedi <gauravdwivedi37@gmail.com>
2024-04-22 18:01:00 +0530

    Function part one

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,30 @@
+// Direct Function Declaration
+
+function myName(){
+    console.log("Gaurav Dwivedi")
+}
+// myName()
+
+function addTwoNumbers(num1, num2 ){ // in the braces called Parameters
+    // console.log(num1+num2)
+    // return num1 + num2
+    let result = num1 + num2
+    return result
+
+}
+
+// addTwoNumbers(3,4) // in the braces called Arguments
+
+const result = addTwoNumbers(3,5)
+// console.log("Result: ", result)
+
+function loginUserMsg(username="Tanuj"){
+    if(!username){
+        console.log("Please Enter Username")
+        return
+    }
+    return `${username} Just Logged In`
+}
+
+const username = loginUserMsg("Gaurav")
+console.log(username)
