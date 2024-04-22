0160e1bb68b9431e201c74530f410f6ff8d28efb
Gaurav Dwivedi <gauravdwivedi37@gmail.com>
2024-04-22 17:15:04 +0530

    Object Part Three

---
+++ 02_basics/05_object.js
@@ -0,0 +1,14 @@
+// Destructuring
+
+
+const courses = {
+    courseName: 'JS in Hindi',
+    price: "999",
+    courseInstructor: "Sir Hitesh"
+}
+
+const {courseInstructor} = courses
+// const {courseInstructor:instructor} = courses
+// console.log(courseInstructor)
+// console.log(instructor)
+
