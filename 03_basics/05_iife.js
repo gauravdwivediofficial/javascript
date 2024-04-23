7d8e931635abb127208ff9bebab84a4e91aea231
Gaurav Dwivedi <gauravdwivedi37@gmail.com>
2024-04-23 17:12:52 +0530

    Immediately Invoked Function Expression

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,25 @@
+//  Immediately Invoked Function Expression(IIFE)
+
+(function radar(){
+    // Named IIFE
+    // console.log(`DB CONNECTED`)
+})(); // here we need to stop this function by adding semicolon
+
+
+((name)=>{
+    // Unnamed IIFE
+    // console.log(`DB CONNECTED ${name}` );
+})("Gaurav");
+
+
+
+
+
+
+
+
+
+
+
+
+
