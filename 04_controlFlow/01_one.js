cc4cbfa18543826402c3073a2d3189380cb2145b
Gaurav Dwivedi <gauravdwivedi37@gmail.com>
2024-04-23 18:27:54 +0530

    control flow

---
+++ 04_controlFlow/01_one.js
@@ -0,0 +1,53 @@
+// ----------------------Control Flow or Logic Flow ---------------------------
+
+// const temperature = 32
+
+// if(temperature < 50){
+//     console.log("Less than 50")
+// }else{
+//     console.log("temperature is greater than 50")
+// }
+
+// const score = 200 
+
+// if(score > 100){
+//     const power = "Fly"
+//     console.log(`User Power is: ${power}`)
+// }
+
+
+// const balance = 1000
+
+// if(balance>500)console.log("test"); // not recomended this type of code
+
+// if (balance < 500){
+//     console.log("Less Than 500")
+// } else if(balance < 750){
+//     console.log("Less than 750");
+// }else if (balance < 900){
+//     console.log("Less than 900");
+// } else{
+//     console.log(`Balance is ${balance}`)
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loginFromGoogle = false
+// const loginFromEmail = true
+
+// if (userLoggedIn && debitCard){
+//     console.log("Allow to buy course")
+// }
+// if (loginFromGoogle || loginFromEmail){
+//     console.log("User Logged In");
+// }
+
+
+
+
+
+
+
+
+
