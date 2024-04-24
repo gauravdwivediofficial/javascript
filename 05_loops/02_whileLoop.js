9452488cd7705a996d2eb38ff1054e00bbb89a1b
Gaurav Dwivedi <gauravdwivedi37@gmail.com>
2024-04-24 16:55:29 +0530

    Do While Loop

---
+++ 05_loops/02_whileLoop.js
@@ -0,0 +1,41 @@
+// While Loop
+
+let index = 2
+
+while(index <= 10){
+    // console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+let myArr = ["Gaurav", "Tanuj", "Shubham"]
+let i = 0
+while (i < myArr.length) {
+    // console.log(`My Name Is ${myArr[i]}`)
+    i = i + 1
+}
+
+
+// Do While Loop
+
+// do {
+    
+// } while (condition);
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
+
+
+
+
+
+
+
+
+
+
+
